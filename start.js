c6ae1be4e9c0d96f8d31d4b9fbbd5fd91c90c551
Satyam3303 <shivamsatyam209@gmail.com>
2022-06-21 20:06:38 +0530

    2nd

--- start.js
+++ start.js
@@ -1,9 +1,17 @@
 function toggle(div_id,hide_id){
 if(div_id==id1){
-    $(div_id).slideDown();
-    $(hide_id).hide();
+     $(div_id).animate({
+        opacity:0
+     },0);
+     $(hide_id).hide();
+     $(div_id).show();
+     $(div_id).animate({
+        opacity:1,
+        width:"99%",
+        height:"99%",
+     },500)
+   
 }
-
 else{
     $(div_id).show();
     $(hide_id).hide();
