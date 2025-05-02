e546f2e4e750527a7591c8c30ec50bb651a4dc6e
SHIVAM SATYAM <shivamsatyam209@gmail.com>
2025-05-02 21:28:23 +0530

    prank changes

---
+++ index.js
@@ -0,0 +1,24 @@
+function toggle(div_id, hide_id) {
+  if (div_id == id1) {
+    $(div_id).animate(
+      {
+        opacity: 0,
+      },
+      0
+    );
+    $(hide_id).hide();
+    $(div_id).show();
+    $(div_id).animate(
+      {
+        opacity: "0.5",
+        width: "99%",
+        height: "99%",
+        display: "flex",
+      },
+      500
+    );
+  } else {
+    $(div_id).show();
+    $(hide_id).hide();
+  }
+}
